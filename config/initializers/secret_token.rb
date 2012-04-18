b2644d1f6242df57bc8741a88fb2423797895c1f
Kamal Ranaweera <ranaweer@ualberta.ca>
2012-04-18 02:46:40 -0600

    Created an empty rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CwrcPlatform::Application.config.secret_token = '60e6e2e437c7cfbd6e37b14a1320b74e512de1821a0d59d53e2cc801c80257b1caf1d553b045b5dfa012019da32a724cec6a07ada78c2c3792945025f3b94573'
