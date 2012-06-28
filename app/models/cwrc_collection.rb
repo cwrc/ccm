4440b71b7c5a0cb81e4857e6a43974b360cd54b0
Kamal Ranaweera <ranaweer@ualberta.ca>
2012-06-28 13:58:44 -0600

    Commented the CwrcCollection.get_child_collections method because its is superseded by the get_children method.

--- app/models/cwrc_collection.rb
+++ app/models/cwrc_collection.rb
@@ -24,27 +24,29 @@ class CwrcCollection < ActiveFedora::Base
     end
   end
   
-  def get_child_collections(recurse = false, result = nil)
-    
-    result = Array.new if result.nil?
-    
-    members.each do |c|
-      if c.is_a?(CwrcCollection)
-        if recurse
-          already_traversed = result.any?{|i| i == c}
-
-          if !already_traversed
-            result.push(c)
-            result = c.get_child_collections(true, result)
-          end
-        else
-          result.push(c)
-        end
-      end #End: if c.is_a?(CwrcCollection)
-    end #End: members.each do |c|
-    
-    return result
-  end
+#
+#  def get_child_collections(recurse = false, result = nil)
+#    
+#    result = Array.new if result.nil?
+#    
+#    members.each do |c|
+#      if c.is_a?(CwrcCollection)
+#        if recurse
+#          already_traversed = result.any?{|i| i == c}
+#
+#          if !already_traversed
+#            result.push(c)
+#            result = c.get_child_collections(true, result)
+#          end
+#        else
+#          result.push(c)
+#        end
+#      end #End: if c.is_a?(CwrcCollection)
+#    end #End: members.each do |c|
+#    
+#    return result
+#  end
+#
   
   def get_children(retCollectionsOnly = false, recurse = false, result = nil)
     
