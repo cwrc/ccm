594443e3fa671988edda380833ef7cafd8a4f07a
Kamal Ranaweera <ranaweer@ualberta.ca>
2012-08-24 15:25:01 -0600

    adding more test cases. Modified the layout age so that it adds a timestamp using javascripts, ater page loads, to support wait function of selenium.

--- spec/integration/item_spec.rb
+++ spec/integration/item_spec.rb
@@ -1,55 +1,64 @@
-require 'ccm_test'
+require 'ccm_test_page'
+
+def create_sample_item_desc(content)
+  '
+  <item>
+    '+content+'
+  </item>
+  '
+end
+
 
 describe "item" do
   it 'creates item', :js => true do
-    test = CcmTest.new
-    test.goto("test/items")
+    page = CcmTestPage.new
+    page.goto("test/items")
     
-    test.page_should_have("Input Information for a New Item")
+    page.page_should_have("Input Information for a New Item")
 
     # creating a scample xml description for a new item    
     content = "item content #{rand(1000)}"
-    desc = test.create_sample_item_desc(content)
-    test.fill_form_field("xml", desc)
+    desc = create_sample_item_desc(content)
+    page.fill_form_field("xml", desc)
 
     #submitting the form
-    test.click("create_button")
+    page.click("create_button")
     
     # making sure that the collection description is corectly saved
-    test.form_filed_should_have("xml", content)
+    page.form_filed_should_have("xml", content)
     
     #close the browser
-    test.quit  
+    page.quit  
   end
 
   it 'creates item in a collection', :js => true do
-    test = CcmTest.new
-    test.goto("test/items")
+    page = CcmTestPage.new
+    page.goto("test/items")
     
-    test.page_should_have("Input Information for a New Item")
+    page.page_should_have("Input Information for a New Item")
 
     # creating a scample xml description for a new item    
     content = "item content #{rand(1000)}"
-    desc = test.create_sample_item_desc(content)
-    test.fill_form_field("xml", desc)
+    desc = create_sample_item_desc(content)
+    page.fill_form_field("xml", desc)
     
     # selecting a parent collection for the new collection
-    object_key_panel = test.get_page_element("potential-parent-list")
+    object_key_panel = page.get_page_element("potential-parent-list")
     collection_links = object_key_panel.find_elements(:class, "entity-id")
     raise "Please create at least one collection to run this test." if collection_links.count == 0
     
     parent_collection_link = collection_links[rand(collection_links.count)]
     parent_id = parent_collection_link.text
     
-    test.fill_form_field("parent", parent_id)
+    page.fill_form_field("parent", parent_id)
 
     #submitting the form
-    test.click("create_button")
+    page.click("create_button")
     
     # making sure that the collection description is corectly saved
-    test.form_filed_should_have("xml", content)
-    test.page_element_text_should_have("current-parent-ids", parent_id)
-    test.quit  
+    page.form_filed_should_have("xml", content)
+    page.page_element_text_should_have("current-parent-ids", parent_id)
+    page.quit  
   end
 
 
