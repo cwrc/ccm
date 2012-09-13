cc2feed8b20bcde2323568dc2ba17b9cbc5abc2a
Kamal Ranaweera <ranaweer@ualberta.ca>
2012-09-13 16:43:45 -0600

    Workflow engine is notified when a workflow stamp is added either whiel saving or via a separate API call.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  
+  require "uri"
+  require "net/http"
+  
   # Adds a few additional behaviors into the application controller 
    include Blacklight::Controller  
 # Adds Hydra behaviors into the application controller 
@@ -28,6 +32,17 @@ class ApplicationController < ActionController::Base
 
   def max_records
     2 ** 31 -1
+  end
+  
+  def notify_workflow_engine(object_id, new_stamp)
+    ##Notifying the Workflow engine
+    url = ENV['workflow_engine_notification_url']
+    
+    uri = URI.parse(url)
+    params = {'id' => object_id, 'stamp' => new_stamp}
+    
+    x = Net::HTTP.post_form(uri, params)
+    x    
   end
   
   # Please be sure to impelement current_user and user_session. Blacklight depends on 
